75d4269acf8e97c538525569a7edbc9c535e28ab
eri-krieg-22 <eri.krieg.22@lehre.mosbach.dhbw.de>
2023-05-04 17:34:39 +0200

    Add weathercode explanation

--- src/weathercode.ts
+++ src/weathercode.ts
@@ -1,7 +1,95 @@
 import {data} from './askWeather';
 import {current_weathercode} from "./dom_utils";
 
+function weathercode_explanation(weathercode:number) {
+    switch(weathercode) {
+        case(0):
+            return "Clear sky";
+
+        case(1):
+            return "Mainly clear";
+
+        case(2):
+            return "Partly cloudy";
+
+        case(3):
+            return "Overcast";
+
+        case(45):
+            return "Fog";
+
+        case(48):
+            return "Depositing rime fog";
+
+        case(51):
+            return "Light drizzle";
+
+        case(53):
+            return "Moderate drizzle";
+
+        case(55):
+            return "Dense drizzle";
+
+        case(56):
+            return "Light freezing drizzle";
+
+        case(57):
+            return "Dense freezing drizzle";
+
+        case(61):
+            return "Slight rain";
+
+        case(63):
+            return "Moderate rain";
+
+        case(65):
+            return "Heavy rain";
+
+        case(66):
+            return "Light freezing rain";
+
+        case(67):
+            return "Heavy freezing rain";
+
+        case(71):
+            return "Slight snow fall";
+
+        case(73):
+            return "Moderate snow fall";
+
+        case(75):
+            return "Heavy snow fall";
+
+        case(77):
+            return "Snow grains";
+
+        case(80):
+            return "Slight rain showers";
+
+        case(81):
+            return "Moderate rain showers";
+
+        case(82):
+            return "Violent rain showers";
+
+        case(85):
+            return "Slight snow showers";
+
+        case(86):
+            return "Heavy snow showers";
+
+        case(95):
+            return "Slight or moderate thunderstorm";
+
+        case(96):
+            return "Thunderstorm with slight hail";
+
+        case(99):
+            return "Thunderstorm with heavy hail";
+    }
+
+}
 export function current_weathercode_function() {
-    current_weathercode.textContent = "Current weathercode: " + data.current_weather.weathercode;
+    current_weathercode.textContent = "Current weathercode: " + data.current_weather.weathercode + " (" + weathercode_explanation(data.current_weather.weathercode) + ")";
     console.log("weathercode");
 }
