c3036a0303191a0e50d388f8e4e3d0ab50145da0
eri-krieg-22 <eri.krieg.22@lehre.mosbach.dhbw.de>
2023-05-04 11:21:29 +0200

    add winddirection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const process_status = document.querySelector("#process_status")as HTMLParagraphElement;
 const current_temperature = document.querySelector("#current_temperature")as HTMLParagraphElement;
 const current_windspeed = document.querySelector("#current_windspeed")as HTMLParagraphElement;
+const current_winddirection = document.querySelector("#current_winddirection")as HTMLParagraphElement;
 const button = document.querySelector("#button")as HTMLButtonElement;
 button.addEventListener("click", askWeather);
 function askWeather() {
@@ -25,6 +26,7 @@ function askWeather() {
                 console.log(data);
                 current_temperature.textContent = "Current temperature: " + data.current_weather.temperature + " Degrees Celsius";
                 current_windspeed.textContent = "Current windspeed: " + data.current_weather.windspeed + " km/h";
+                current_winddirection.textContent = "Current winddirection: " + data.current_weather.winddirection + " degrees"
             } else {
                 console.log(`Error: ${xhr.status}`);
             }
